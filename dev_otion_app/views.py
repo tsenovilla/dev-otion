0a6f40df05648ef030b95f6f2f28cb4e35b9867e
Tomás Senovilla Polo <tspscgs@gmail.com>
2023-07-12 14:17:39 +0200

    Adding language switching and first translations

--- dev_otion_app/views.py
+++ dev_otion_app/views.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import activate
 from django.views.generic import DetailView, ListView
@@ -13,6 +12,7 @@ class Dev_otion_ListView(ListView):
     def get_context_data(self):
         context = super().get_context_data()
         context['year'] = timezone.now().strftime('%Y')
+        context['current_url'] = self.request.path[4:]
         return context
 
 class IndexView(Dev_otion_ListView):
