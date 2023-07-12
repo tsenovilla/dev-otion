e120ab8393a8a830b83fbe0ffb750549eea57365
Tomás Senovilla Polo <tspscgs@gmail.com>
2023-07-12 18:07:52 +0200

    Adding code highlighting for entries

--- src/js/dev_otion_app/app.js
+++ src/js/dev_otion_app/app.js
@@ -1,4 +1,5 @@
 import menu_animation from "./menu_animation_helper";
+import code_highlighting from "./code_highlighting";
 
 const menu = document.querySelector('.menu');
 document.addEventListener('DOMContentLoaded',menu_animation(menu));
---
+++ src/js/dev_otion_app/code_highlighting.js
@@ -0,0 +1,9 @@
+/**
+ * This function changes the innerHTML of each piece of code written in the article (encapsulated under [code][/code] marks in the database) in order to apply code highlighting
+ * @param {HTMLElement} article
+ */
+export default function(article)
+{
+    article.innerHTML = article.innerHTML.replace(/\[code\](.*?)\[\/code\]/gs,'<pre><code>$1</code></pre>');
+    hljs.highlightAll();
+}
