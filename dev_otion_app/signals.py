1c4ee1eb475a4e5df474a5441cb309a13163cda2
Tomás Senovilla Polo <tspscgs@gmail.com>
2023-08-04 18:13:31 +0200

    Solving topic deletion in bulk images problem

--- dev_otion_app/signals.py
+++ dev_otion_app/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from .models import Topics
 import os
 
-## Override of post_delete signal for Topics, in order to delete images after deleting a db entry. We have to override the signal instead of the delete method in order to correctly delete images if we delete entries in bulks.
+## Override of post_delete signal for Topics, in order to delete images after deleting a db entry. We have to override the signal instead of the delete method in order to correctly delete images if we delete entries in bulk.
 @receiver(post_delete, sender=Topics)
 def topic_post_delete(sender, instance,**kwargs):
     former_image = instance.image.path
