8d8fd447a5144add0a8044f3a071ee04401c951a
Tomás Senovilla Polo <tspscgs@gmail.com>
2023-08-10 19:24:05 +0200

    Search bar case insensitive

--- dev_otion_app/search_API.py
+++ dev_otion_app/search_API.py
@@ -16,19 +16,19 @@ class SearchView(View):
         match lang:
             case 'en':
                 for word in input.split():
-                    query&=Q(title_english__contains = word)
+                    query&=Q(title_english__icontains = word)
                 entrys = Entry.objects.filter(query).order_by('-pub_date')[:4] ## We return entries ordered by pub_date, with a maximum of 4. This is to not overload the entries showed to the user, as it may do a better search the results showed are not exactly the desired ones
                 for entry in entrys:
                     response[entry.title_english] = reverse('dev_otion_app:entry', kwargs = {'url': entry.url_english})
             case 'es':
                 for word in input.split():
-                    query&=Q(title_spanish__contains = word)
+                    query&=Q(title_spanish__icontains = word)
                 entrys = Entry.objects.filter(query).order_by('-pub_date')[:4]
                 for entry in entrys:
                     response[entry.title_spanish] = reverse('dev_otion_app:entry', kwargs = {'url': entry.url_spanish})
             case 'fr':
                 for word in input.split():
-                    query&=Q(title_french__contains = word)
+                    query&=Q(title_french__icontains = word)
                 entrys = Entry.objects.filter(query).order_by('-pub_date')[:4]
                 for entry in entrys:
                     response[entry.title_french] = reverse('dev_otion_app:entry', kwargs = {'url': entry.url_french})
