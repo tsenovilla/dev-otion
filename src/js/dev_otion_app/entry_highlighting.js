b56aeadbda66fdcf580b57dc497dce53ec9d8767
Tomás Senovilla Polo <tspscgs@gmail.com>
2023-10-08 10:11:08 +0200

    Adding Prism.js for code highlighting instead of Highlights.js

--- src/js/dev_otion_app/entry_highlighting.js
+++ src/js/dev_otion_app/entry_highlighting.js
@@ -1,9 +1,8 @@
 /**
- * This function changes the innerHTML of each piece of code written in the article (encapsulated under [code][/code] marks in the database) in order to apply code highlighting.
+ * This function changes the innerHTML of each piece of code written in the article (encapsulated under [code language][/code] marks in the database) in order to apply code highlighting for the written language
  * @param {HTMLElement} article
  */
 export default function(article)
 {
-    article.innerHTML = article.innerHTML.replace(/<p>\[code\]<\/p>(.*?)<p>\[\/code\]<\/p>/gs,'<pre><code>$1</code></pre>');
-    hljs.highlightAll();
+    article.innerHTML = article.innerHTML.replace(/<p>\[code (.*?)\]<\/p>(.*?)<p>\[\/code\]<\/p>/gs,'<pre><code class="language-$1">$2</code></pre>');
 }
